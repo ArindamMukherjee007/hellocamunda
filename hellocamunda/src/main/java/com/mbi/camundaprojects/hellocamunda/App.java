a68f3e16ee345aebd0e71a6c268d739ab662c498
Anupam <Anupam@Anupam-PC>
2020-02-08 18:51:53 +0530

    release 2020 : new syso added

--- hellocamunda/src/main/java/com/mbi/camundaprojects/hellocamunda/App.java
+++ hellocamunda/src/main/java/com/mbi/camundaprojects/hellocamunda/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World1!" );
+        System.out.println("this is for 2002 release ");
     }
 }
